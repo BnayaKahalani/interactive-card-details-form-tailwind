0396644c7dc0a6020d2e2f42f58363e8994aa80d
Bnaya Kahalani <bnaya50@gmail.com>
2023-01-23 13:12:23 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-red-300">
+      <h1>Hello from app</h1>
+    </div>
+  )
+}
+
+export default App
